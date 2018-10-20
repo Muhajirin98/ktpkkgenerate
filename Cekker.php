6232b814aa5d204a46a88d95f44652d0461342f7
Muhajirin98 <44298732+Muhajirin98@users.noreply.github.com>
2018-10-20 17:28:46 +0700

    Create Cekker.php

---
+++ Cekker.php
@@ -0,0 +1,5 @@
+<? php 
+print  " [Aplikasi periksa No.KTP & KK] \ n \ n " ;
+ print  " Tautan unduh nya: https://goo.gl/vqbvtA \ n \ n " ;
+print  " Terima kasih kepada: BabbyCyberTeam || SGB-TEAM || JNCK MEDIA || And You \ n " ;
+? >
